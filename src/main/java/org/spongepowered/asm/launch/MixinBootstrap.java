a0b01d3c9c2c898d9208d279b9f1c5ebea906ace
Mumfrey <adam@eq2.co.uk>
2015-05-05 22:45:43 +0100

    Exclude new ASM package in bootstrap

--- src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
+++ src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
@@ -71,7 +71,7 @@ public abstract class MixinBootstrap {
     // Consts
     private static final String LAUNCH_PACKAGE = "org.spongepowered.asm.launch.";
     private static final String MIXIN_PACKAGE = "org.spongepowered.asm.mixin.";
-    private static final String ASM_PACKAGE = "org.objectweb.asm.";
+    private static final String ASM_PACKAGE = "org.spongepowered.asm.lib.";
     private static final String TRANSFORMER_PROXY_CLASS = MixinBootstrap.MIXIN_PACKAGE + "transformer.MixinTransformer$Proxy";
     
     /**
