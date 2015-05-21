771746185f0c268be7127c2fee6c49aaf55c043b
Mumfrey <adam@eq2.co.uk>
2015-05-21 19:20:54 +0100

    Version bump to 0.4.3

--- src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
+++ src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
@@ -60,7 +60,7 @@ public abstract class MixinBootstrap {
     /**
      * Subsystem version
      */
-    public static final String VERSION = "0.4.2";
+    public static final String VERSION = "0.4.3";
     
     /**
      * Blackboard key where the subsystem version will be stored to indicate
