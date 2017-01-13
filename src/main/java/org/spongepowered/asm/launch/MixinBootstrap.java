27f40c7b134cab4cc565ab3c1255d4af20ffc516
Mumfrey <adam@eq2.co.uk>
2017-01-13 16:04:35 +0000

    Bump version to 0.6.5

--- src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
+++ src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
@@ -62,7 +62,7 @@ public abstract class MixinBootstrap {
     /**
      * Subsystem version
      */
-    public static final String VERSION = "0.6.4";
+    public static final String VERSION = "0.6.5";
     
     // Consts
     private static final String LAUNCH_PACKAGE = "org.spongepowered.asm.launch.";
