7568a39941723cccc1a16751af4158d95c11a697
Mumfrey <adam@eq2.co.uk>
2019-11-09 20:03:40 +0000

    Remove preview suffix from internal version string

--- src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
+++ src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
@@ -61,7 +61,7 @@ public abstract class MixinBootstrap {
     /**
      * Subsystem version
      */
-    public static final String VERSION = "0.8-preview";
+    public static final String VERSION = "0.8";
     
     /**
      * Log all the things
