8dbd87622573905e6d32f6d9f28e7b45cb590ec6
Mumfrey <adam@eq2.co.uk>
2021-09-08 17:13:08 +0100

    Bump version to 0.8.5-SNAPSHOT

--- src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
+++ src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
@@ -65,7 +65,7 @@ public abstract class MixinBootstrap {
     /**
      * Subsystem version
      */
-    public static final String VERSION = "0.8.4";
+    public static final String VERSION = "0.8.5";
     
     /**
      * Transformer factory 
